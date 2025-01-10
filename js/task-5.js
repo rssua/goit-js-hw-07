b4b2900e990ab9b4167ca7b51c9828d7d015a433
Andrii Rudenko <k615wadem@mozmail.com>
2025-01-10 06:54:00 +0200

    all right

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777216)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+const changeColor = document.querySelector('.change-color');
+
+changeColor.addEventListener('click', () => {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = color.textContent;
+});
